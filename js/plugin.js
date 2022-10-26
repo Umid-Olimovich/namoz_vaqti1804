d87b07b7531ca3c8e608c4707c775d08a870b5b5
unknown <https://github.com/Umid-Olimovich>
2022-10-26 15:57:35 +0300

    first commit

---
+++ js/plugin.js
@@ -0,0 +1,24 @@
+
+let $ = function (select) {
+    return document.querySelector(select);
+}
+
+let $a = function (select) {
+    return document.querySelectorAll(select);
+}
+
+
+
+const createEl = function (tagName, className, content, parents) {
+    const newElement = document.createElement(tagName);
+    if (className) {
+        newElement.setAttribute("class", className);
+    }
+    if (content) {
+        newElement.innerHTML = content;
+    }
+    if (parents) {
+        return parents.appendChild(newElement);
+    }
+    return newElement;
+}
